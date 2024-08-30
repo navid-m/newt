90fe77eed2ba326e04da79a3780175a1129619be
Navid M <navidmomtahen@gmail.com>
2024-08-30 17:26:02 +0100

    structure

--- src/nytdlp.nim
+++ src/nytdlp.nim
@@ -1,22 +1,11 @@
 import std/uri
 import std/net
+import nytdlp/randoms
 import httpclient
 import json
 import strformat
 import os
 import strutils
-import random
-
-
-# Generate random agent
-proc randomUserAgent(): string =
-  result = sample(@[
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1"
-  ])
 
 
 # Get video info using API
@@ -34,7 +23,9 @@ proc getVideoInfo(videoId: string, invidiousInstanceUrl: string = "invidious.per
   var response: string
 
   try:
-    response = client.getContent(fmt"http://{invidiousInstanceUrl}/api/v1/videos/{encodeUrl(videoId)}")
+    let url = fmt"http://{invidiousInstanceUrl}/api/v1/videos/{encodeUrl(videoId)}"
+    echo url
+    response = client.getContent(url)
   except HttpRequestError as e:
     echo "Error fetching video info: ", e.msg
     quit(1)
@@ -42,68 +33,82 @@ proc getVideoInfo(videoId: string, invidiousInstanceUrl: string = "invidious.per
   return parseJson(response)
 
 
-# Get the video URL from the JSON node
-proc extractVideoUrl(videoInfo: JsonNode): string =
+# Get highest quality video stream.
+proc getVideo(videoInfo: JsonNode): JsonNode =
+  var bestStream: JsonNode = nil
   for stream in videoInfo["adaptiveFormats"].items:
-    if stream["type"].getStr().contains("video/mp4"):
-      return stream["url"].getStr()
+    if stream["type"].getStr().startsWith("video/") and stream.hasKey("audioQuality"):
+      if bestStream.isNil or (stream["bitrate"].getInt() > bestStream[
+          "bitrate"].getInt()):
+        bestStream = stream
+  if bestStream.isNil:
+    raise newException(ValueError, "No video found")
+  return bestStream
 
-  raise newException(ValueError, "No valid video URL found")
+
+# Get highest quality audio stream.
+proc getAudio(videoInfo: JsonNode): JsonNode =
+  var bestStream: JsonNode = nil
+  for stream in videoInfo["adaptiveFormats"].items:
+    if stream["type"].getStr().startsWith("audio/"):
+      if bestStream.isNil or (stream["bitrate"].getInt() > bestStream[
+          "bitrate"]
+        .getInt()
+      ):
+        bestStream = stream
+  if bestStream.isNil:
+    raise newException(ValueError, "No audio found")
+  return bestStream
 
 
 # Download the fucking thing
-proc downloadVideo(url: string, outputPath: string) =
+proc downloadFile(url: string, outputPath: string) =
   let client = newHttpClient(userAgent = randomUserAgent())
 
   client.headers.add("Accept", "*/*")
   client.headers.add("Referer", "http://google.com.bz/")
   client.headers.add("Accept-Language", "en-US,en;q=0.9")
 
-  var videoContent: string
+  var fileContent: string
 
   try:
-    videoContent = client.getContent(url)
+    fileContent = client.getContent(url)
   except HttpRequestError as e:
-    echo "Error downloading video: ", e.msg
+    echo "Error downloading file: ", e.msg
     quit(1)
 
-  writeFile(outputPath, videoContent)
-
-
-# Mix video and audio files using ffmpeg
-proc mergeAudioAndVideo(
-    videoPath: string,
-    audioPath: string,
-    outputPath: string
-  ) =
-  discard execShellCmd(fmt"ffmpeg -i {videoPath} -i {audioPath} -c copy {outputPath} -hide_banner -loglevel error")
+  writeFile(outputPath, fileContent)
 
 
 # Run the CLI
 proc main() =
   if paramCount() < 1:
-    echo "Usage: nytdlp <YouTube video URL>"
+    echo "Usage: nytdlp [-v|-a] <YouTube video URL>"
     quit(1)
 
-  let videoInfo = getVideoInfo(paramStr(1).split("=")[^1])
-  let videoFile = "raw_video.mp4"
+  var extractedVideoId: seq[string]
 
-  downloadVideo(extractVideoUrl(videoInfo), videoFile)
+  try:
+    extractedVideoId = paramStr(2).split("=")
+  except:
+    extractedVideoId = paramStr(1).split("=")
 
-  echo "Downloaded video to ", videoFile
+  echo extractedVideoId
+  let videoInfo = getVideoInfo(extractedVideoId[^1])
+  echo videoInfo
 
-  if videoInfo.hasKey("adaptiveFormats"):
-    let audioFile = "audio_output.m4a"
-    let outputFile = "video_output.mp4"
+  if paramCount() == 1 or paramStr(1) == "-a":
+    downloadFile(getAudio(videoInfo)["url"].getStr(), "output.opus")
 
-    downloadVideo(videoInfo["adaptiveFormats"][0]["url"].getStr(), audioFile)
-    mergeAudioAndVideo(videoFile, audioFile, outputFile)
-
-    echo "Downloaded audio to: ", audioFile
-    echo "Merged video and audio to: ", outputFile
+  elif paramStr(1) == "-v":
+    downloadFile(getVideo(videoInfo)["url"].getStr(), "output.mp4")
 
   else:
-    echo "(Video does not have a separate audio stream)"
+    echo "Invalid option provided."
+    echo "Usage: nytdlp [-v|-a] <YouTube video URL>"
+    quit(1)
+
+  echo "Downloaded to current directory."
 
 
 main()
---
+++ src/nytdlp/randoms.nim
@@ -0,0 +1,19 @@
+import random
+
+# Generate random agent
+proc randomUserAgent*(): string =
+  result = sample(@[
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1"
+  ])
+
+proc randomIvInstance*(): string =
+  result = sample(@[
+    "iv.nboeck.de",
+    "yewtu.be",
+    "invidious.adminforge.de",
+    "iv.nboeck.de"
+  ])
