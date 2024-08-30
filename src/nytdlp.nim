fd5809a865780ea383dcf996c559408df542a532
Navid M <navidmomtahen@gmail.com>
2024-08-30 14:28:37 +0100

    simplify

--- src/nytdlp.nim
+++ src/nytdlp.nim
@@ -9,18 +9,16 @@ import std/net
 
 # Generate random agent
 proc randomUserAgent(): string =
-  let userAgents = @[
+  result = sample(@[
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
     "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
     "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1"
-  ]
-  result = sample(userAgents)
+  ])
 
 # Get video info using API
 proc getVideoInfo(videoId: string, invidiousInstanceUrl: string = "iv.ggtyler.dev"): JsonNode =
-  let apiUrl = fmt"http://{invidiousInstanceUrl}/api/v1/videos/{encodeUrl(videoId)}"
   let client = newHttpClient()
   let userAgentToUse = randomUserAgent()
 
@@ -33,7 +31,7 @@ proc getVideoInfo(videoId: string, invidiousInstanceUrl: string = "iv.ggtyler.de
 
   var response: string
   try:
-    response = client.getContent(apiUrl)
+    response = client.getContent(fmt"http://{invidiousInstanceUrl}/api/v1/videos/{encodeUrl(videoId)}")
   except HttpRequestError as e:
     echo "Error fetching video info: ", e.msg
     quit(1)
@@ -80,25 +78,24 @@ proc main() =
     echo "Usage: nytdlp <YouTube video URL>"
     quit(1)
 
-  let videoUrl = paramStr(1)
-  let videoId = videoUrl.split("=")[^1]
-  let videoInfo = getVideoInfo(videoId)
+  let videoInfo = getVideoInfo(paramStr(1).split("=")[^1])
   let videoFile = "video.mp4"
-  let videoStreamUrl = extractVideoUrl(videoInfo)
 
-  downloadVideo(videoStreamUrl, videoFile)
+  downloadVideo(extractVideoUrl(videoInfo), videoFile)
 
   echo "Downloaded video to ", videoFile
 
   if videoInfo.hasKey("adaptiveFormats"):
-    let audioStreamUrl = videoInfo["adaptiveFormats"][0]["url"].getStr()
-    let audioFile = "audio.m4a"
-    downloadVideo(audioStreamUrl, audioFile)
-    echo "Downloaded audio to ", audioFile
-    let outputFile = "output.mp4"
+    let audioFile = "audio_output.m4a"
+    let outputFile = "video_output.mp4"
+
+    downloadVideo(videoInfo["adaptiveFormats"][0]["url"].getStr(), audioFile)
     mergeAudioAndVideo(videoFile, audioFile, outputFile)
-    echo "Merged video and audio to ", outputFile
+
+    echo "Downloaded audio to: ", audioFile
+    echo "Merged video and audio to: ", outputFile
+
   else:
-    echo "Video does not have a separate audio stream, saved as ", videoFile
+    echo "(Video does not have a separate audio stream)"
 
 main()
