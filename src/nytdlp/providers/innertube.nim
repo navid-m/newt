2c11d5897f24bfbf6cf321cec4dbb9efc694e057
Navid M <navidmomtahen@gmail.com>
2024-09-01 02:15:01 +0100

    fix vid issue

--- src/nytdlp/providers/innertube.nim
+++ src/nytdlp/providers/innertube.nim
@@ -50,8 +50,7 @@ proc getVideoInfo(videoId: string, client: HttpClient): JsonNode =
 proc getVideo(videoInfo: JsonNode): JsonNode =
   ## Get highest quality video stream.
   var bestStream: JsonNode = nil
-
-  for stream in videoInfo["streamingData"]["adaptiveFormats"].items:
+  for stream in videoInfo["streamingData"]["formats"].items:
     if stream["mimeType"].getStr().startsWith("video/") and stream.hasKey("audioQuality"):
       if bestStream.isNil or (
         stream["bitrate"].getInt() > bestStream["bitrate"].getInt()
