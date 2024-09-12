a418aa71b06f8216e22118f8148b368a187585a2
Navid M <navidmomtahen@gmail.com>
2024-09-12 14:44:51 +0100

    fmt

--- src/nytdlp/downloads/dl.nim
+++ src/nytdlp/downloads/dl.nim
@@ -1,16 +1,18 @@
-import ../providers/[ivtube, innertube]
+import
+  ../providers/[ivtube, innertube],
+  ../diagnostics/logger
 
 
 proc downloadStream(url: string, isAudio: bool) =
   try:
     downloadInnerStream(url, isAudio)
   except:
-    echo "Falling back to IV, as Innertube request failed. Only video can be downloaded this way"
-    echo "Details: ", getCurrentException().msg
+    LogInfo(
+      "Falling back to IV, as Innertube request failed. Only video can be downloaded this way",
+      "Details:", getCurrentException().msg
+    )
     downloadIvStream(url)
 
-proc downloadAudio*(url: string) =
-  downloadStream(url, true)
 
-proc downloadVideo*(url: string) =
-  downloadStream(url, false)
+proc downloadAudio*(url: string) = downloadStream(url, true)
+proc downloadVideo*(url: string) = downloadStream(url, false)
