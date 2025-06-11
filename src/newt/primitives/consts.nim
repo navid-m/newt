b54066fd0b6c6a1a052a29bcd71e969bda2feeab
Navid M <navidmomtahen@gmail.com>
2025-06-11 08:03:44 +0100

    fmt

--- src/newt/primitives/consts.nim
+++ src/newt/primitives/consts.nim
@@ -4,7 +4,6 @@ import json
 let Agent* = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
 let DownloaderAgent* = "com.google.android.youtube/18.11.34 (Linux; U; Android 11) gzip"
 let APIKey* = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
-
 let ClientContext* = %*{
   "client": {
     "hl": "en",
